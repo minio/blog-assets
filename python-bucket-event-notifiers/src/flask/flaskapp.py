6a6559ebd8375a393ad4535aebf273c12e40d398
David <104325852+Cdaprod@users.noreply.github.com>
2023-12-17 13:47:26 -0500

    Update and rename app.py to flaskapp.py
    Changed endpoints to use container hostnames rather than ‘localhost’

--- python-bucket-event-notifiers/src/flask/flaskapp.py
+++ python-bucket-event-notifiers/src/flask/flaskapp.py
@@ -48,7 +48,7 @@ redis_client = redis.Redis(
     host=RedisClientConfig.host, 
     port=RedisClientConfig.port)
 
-# This is where we start the Flask App and create API endpoints consisting of: @app.route (Route, RouteMethod)
+# This is where we start the Flask App and create API endpoints consisting of: @app.route ( TheRoute, TheRoutesMethod)
 app = Flask(__name__)
 
 @app.route('/event', methods=['POST'])
