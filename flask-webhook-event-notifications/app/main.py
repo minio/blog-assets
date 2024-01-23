f39df78b73418fe6980f3ff6d9d6c052c7791b2e
David <104325852+Cdaprod@users.noreply.github.com>
2024-01-23 12:31:05 -0500

    Update main.py

--- flask-webhook-event-notifications/app/main.py
+++ flask-webhook-event-notifications/app/main.py
@@ -1,23 +1,4 @@
-import json
-import psycopg2
 from flask import Flask, jsonify, request
-from minio import Minio
-
-# Initialize MinIO and PostgreSQL clients
-minio_client = Minio(
-    endpoint='minio:9000',
-    access_key='minio',
-    secret_key='minio123',
-    secure=True
-)
-
-pg_conn = psycopg2.connect(
-    host='postgres',
-    port=5432,
-    user='myuser',
-    password='mypassword',
-    dbname='postgres'
-)
 
 # Flask app initialization
 app = Flask(__name__)
@@ -26,33 +7,9 @@ app = Flask(__name__)
 @app.route('/minio-event', methods=['POST'])
 def handle_minio_event():
     event_data = request.json
-    try:
-        with pg_conn.cursor() as cur:
-            for record in event_data['Records']:
-                try:
-                    # Extract object key directly from record
-                    object_key = record['s3']['object']['key']
-                except KeyError as e:
-                    print(f"Key error: {e}")
-                    continue
-
-                # Store the entire record as JSON
-                json_data = json.dumps(record)
-
-                cur.execute("""
-                    INSERT INTO events (key, value) 
-                    VALUES (%s, %s)
-                    ON CONFLICT (key) 
-                    DO UPDATE SET value = EXCLUDED.value;
-                    """, (object_key, json_data))
-            pg_conn.commit()
-    except Exception as e:
-        print(f"Error: {e}")
-        pg_conn.rollback()
-    finally:
-        pg_conn.close()
-
-    return "Event processed", 200
+    # Using Flask's default logger to log the event data
+    app.logger.info(f"Received MinIO event: {event_data}")
+    return jsonify(event_data), 200
 
 
 @app.route('/hello', methods=['GET'])
