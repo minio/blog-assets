86db4e9840e86f89f8bb3306ef7b8fa88b25effe
David <104325852+Cdaprod@users.noreply.github.com>
2024-03-22 14:29:13 -0400

    Update entrypoint.sh

--- adv-GitOps-strategies/minio/entrypoint.sh
+++ adv-GitOps-strategies/minio/entrypoint.sh
@@ -10,8 +10,15 @@ sleep 5
 
 # Set up alias and create bucket
 mc alias set myminio http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
-mc mb myminio/weaviate-backups
-mc mb myminio/cda-datasets
+
+if ! mc ls myminio/weaviate-backups; then
+  mc mb myminio/weaviate-backups
+fi
+
+if ! mc ls myminio/cda-datasets; then
+  mc mb myminio/cda-datasets
+fi
+
 
 # Keep the script running to prevent the container from exiting
 tail -f /dev/null
